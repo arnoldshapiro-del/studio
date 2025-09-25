171ab57c1424d1bcd0b6be87fc30452dcdf9944a
arnold shapiro <arnold.shapiro@gmail.com>
2025-09-25 20:15:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4865781067-c8917",
+  "appId": "1:544673505997:web:9209c269a702c383ecbf84",
+  "apiKey": "AIzaSyDfo7pJnHmucL-N1q9oSh3EJaABP8l_MHY",
+  "authDomain": "studio-4865781067-c8917.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "544673505997"
+};
